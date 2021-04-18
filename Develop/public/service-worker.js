579fb56a0789ef9b7f63e14c4c3c64ad6cf62e2f
Bhumisha Dave <bhumishadave@yahoo.com>
2021-04-17 18:15:45 -0700

    New branch for Budget tracker

---
+++ Develop/public/service-worker.js
@@ -0,0 +1,108 @@
+// // Uncomment the lines below to set up the cache files
+console.log("Fro Service worker");
+const CACHE_NAME = 'budgetTracker-cache-v1';
+const DATA_CACHE_NAME = 'budgetTracker_data-cache-v1';
+
+const FILES_TO_CACHE = [
+  '/index.html',
+  '/manifest.json',
+  '/js/index.js',
+  '/js/idb.js',
+  '/assets/css/styles.css',
+  '/assets/icons/icon-72x72.png',
+  '/assets/icons/icon-96x96.png',
+  '/assets/icons/icon-128x128.png',
+  '/assets/icons/icon-144x144.png',
+  '/assets/icons/icon-152x152.png',
+  '/assets/icons/icon-192x192.png',
+  '/assets/icons/icon-384x384.png',
+  '/assets/icons/icon-512x512.png',
+ 
+ ];
+
+// Install the service worker
+self.addEventListener('install',function(event){
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(cache => {
+            console.log('Your files were pre-cached successfully!');
+            return cache.addAll(FILES_TO_CACHE);
+        })
+    );
+    self.skipWaiting();
+});
+
+
+// Activate the service worker and remove old data from the cache
+self.addEventListener('activate',function(event){
+    event.waitUntil(
+        caches.keys().then(keyList => {
+            return Promise.all(
+                keyList.map(key => {
+                    if(key !== CACHE_NAME && key!= DATA_CACHE_NAME){
+                        console.log("removing old cache data",key);
+                        return caches.delete(key)
+                    }
+                })
+            );
+        })
+    );
+    self.clients.claim();
+});
+
+// Intercept fetch requests
+self.addEventListener('fetch', function(evt) {
+    if (evt.request.url.includes('/api/')) {
+        evt.respondWith(
+          caches
+            .open(DATA_CACHE_NAME)
+            .then(cache => {
+              return fetch(evt.request)
+                .then(response => {
+                  // If the response was good, clone it and store it in the cache.
+                  if (response.status === 200) {
+                    cache.put(evt.request.url, response.clone());
+                  }
+    
+                  return response;
+                })
+                .catch(err => {
+                  // Network request failed, try to get it from the cache.
+                  return cache.match(evt.request);
+                });
+            })
+            .catch(err => console.log(err))
+        );
+    
+        return;
+      }
+      evt.respondWith(
+        fetch(evt.request).catch(function() {
+          return caches.match(evt.request).then(function(response) {
+            if (response) {
+              return response;
+            } else if (evt.request.headers.get('accept').includes('text/html')) {
+              // return the cached home page for all requests for html pages
+              return caches.match('/');
+            }
+          });
+        })
+      );
+});
+
+// self.addEventListener('fetch', function (e) {
+//   console.log('fetch request : ' + e.request.url)
+//   e.respondWith(
+//     caches.match(e.request).then(function (request) {
+//       if (request) { // if cache is available, respond with cache
+//         console.log('responding with cache : ' + e.request.url)
+//         return request
+//       } else {       // if there are no cache, try fetching request
+//         console.log('file is not cached, fetching : ' + e.request.url)
+//         return fetch(e.request)
+//       }
+
+//       // You can omit if/else for console.log & put one line below like this too.
+//       // return request || fetch(e.request)
+//     })
+//   )
+// })
