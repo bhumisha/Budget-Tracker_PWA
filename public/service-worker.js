3b64abcd3bc495f7b728d2c7495587404dc4b9c9
Bhumisha Dave <bhumishadave@yahoo.com>
2021-04-18 12:04:41 -0700

    Changed path of files in service worker as I changed folder structure

--- public/service-worker.js
+++ public/service-worker.js
@@ -4,19 +4,19 @@ const CACHE_NAME = APP_PREFIX + VERSION;
 const FILES_TO_CACHE = [
     "/",
     "./index.html",
-    "./css/styles.css",
+    "./assets/css/styles.css",
     "./js/index.js",
     "./js/idb.js",
     "./manifest.json",
     "/api/transaction",
-    "./icons/icon-72x72.png",
-    "./icons/icon-96x96.png",
-    "./icons/icon-128x128.png",
-    "./icons/icon-144x144.png",
-    "./icons/icon-152x152.png",
-    "./icons/icon-192x192.png",
-    "./icons/icon-384x384.png",
-    "./icons/icon-512x512.png"
+    "./assets/icons/icon-72x72.png",
+    "./assets/icons/icon-96x96.png",
+    "./assets/icons/icon-128x128.png",
+    "./assets/icons/icon-144x144.png",
+    "./assets/icons/icon-152x152.png",
+    "./assets/icons/icon-192x192.png",
+    "./assets/icons/icon-384x384.png",
+    "./assets/icons/icon-512x512.png"
 ];
 
 
